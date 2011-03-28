297896f7df83c584b3f002ef8787874b974d5255
Jay Hannah <jay.hannah@iinteractive.com>
2011-03-28 17:12:19 -0500

    Checkpoint.

--- lib/KiokuX/Model/UserAccounts/User.pm
+++ lib/KiokuX/Model/UserAccounts/User.pm
@@ -6,5 +6,20 @@ has name => (
    isa => 'Str',
 );
 
+has identities => (
+   traits  => ['Array'],
+   is      => 'rw',
+   # TODO Hmm... what I really want is:
+   # isa    => 'ArrayRef[KiokuX::Model::UserAccounts::Identity]',
+   # but that's a Role not a Class... hmmm...    --jhannah 20110328
+   isa     => 'ArrayRef[Narwhal::Identity]',
+   default => sub { [] },
+   handles => {
+      all_identities    => 'elements',
+      filter_identities => 'grep',
+      add_identity      => 'push',
+   },
+);
+
 no Moose::Role;
 1;
